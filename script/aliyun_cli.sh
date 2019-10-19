c5c2b845292a4df0985936ff53f31d86386f3d03
wjq <wjq@wjqdeMacBook-Air.local>
2019-10-19 18:18:38 +0800

    Add enable venv

--- script/aliyun_cli.sh
+++ script/aliyun_cli.sh
@@ -3,7 +3,11 @@
 path=$(dirname $0)
 
 src_path=${path%%script*}'src'
+source_cmd='source '${path%%script*}'.venv/bin/activate'
 cm_path=$src_path'/crawler/base/CrawlerManager.py'
+
+echo $source_cmd
+$source_cmd
 
 export_cmd='export PYTHONPATH='$src_path
 $export_cmd
